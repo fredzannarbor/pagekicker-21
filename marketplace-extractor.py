51052cb05eb51a5fccc295a243094c747a34ea8e
Fred Zimmerman <fred@pagekicker.com>
2016-08-25 18:20:47 -0400

    adds 21 marketplace analyzer

---
+++ marketplace-extractor.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 19:41:44 2016
+
+@author: fred
+"""
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open("/home/fred/Dropbox/Downloads/21mkt"))
+
+BeautifulSoup('<span class="price-amount">')
+spans = soup.find_all('span', attrs={'class':'price-amount'})
+for span in spans:
+    print(span.string)
+
+
+titles = soup.find_all('h3', attrs={'class':'data-market-endpoint-title'})
+for title in titles:
+    print(title.string)
+
+sources=soup.findAll('a',{"data-keyword":True})
+for source in sources:
+    print(source['data-keyword'])
