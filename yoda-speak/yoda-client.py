36c3af974ca9e202ebfee740dcc39696f72ac00b
Fred Zimmerman <fred@pagekicker.com>
2016-08-28 16:36:42 -0400

    adds working Yoda Speaks app

---
+++ yoda-speak/yoda-client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+from two1.wallet import Wallet
+from two1.bitrequests import BitTransferRequests
+
+import argparse
+import urllib
+
+parser = argparse.ArgumentParser()
+parser.add_argument('s', help = 'sentence to be translated')
+args = parser.parse_args()
+sentence = args.s
+#print(sentence)
+# set up bitrequest client for BitTransfer requests
+wallet = Wallet()
+requests = BitTransferRequests(wallet)
+
+# server address
+server_url = 'http://localhost:5000/'
+
+def buy_yoda(sentence):
+
+    sel_url = server_url+'buy?sentence='
+
+    sentence = urllib.parse.quote_plus(sentence)
+    response = requests.get(url=sel_url+sentence)
+    print(response.text)
+
+if __name__ == '__main__':
+    buy_yoda(sentence)
