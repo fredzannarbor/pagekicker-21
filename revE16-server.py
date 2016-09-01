7b4838d324b5a031ac9fbc402392db259554a4b6
James Poole <poole.james@gmail.com>
2016-09-01 16:53:11 -0400

    Initial check in for RevE16 files

--- revE16-server.py
+++ revE16-server.py
@@ -7,15 +7,13 @@ import psutil
 import subprocess
 import os
 import yaml
-import ipaddress
 
 from flask import Flask
-from flask import request
 
 from two1.wallet.two1_wallet import Wallet
 from two1.bitserv.flask import Payment
 
-from statsE16 import statsE16
+from revE16 import RevE16
 
 app = Flask(__name__)
 
@@ -40,13 +38,14 @@ def manifest():
 @app.route('/')
 @payment.required(5)
 def measurement():
-    """ Queries the local device for stats details
+    """
+    Queries the local device for revenue stats details.
 
     Returns: HTTPResponse 200 with a json containing the stats info.
     HTTP Response 400 if there is an error reading the stats.
     """
     try:
-        data = statsE16()
+        data = RevE16.getRevenue(1)
         response = json.dumps(data, indent=4, sort_keys=True)
         return response
     except ValueError as e:
@@ -57,11 +56,13 @@ if __name__ == '__main__':
     import click
 
     @click.command()
-    @click.option("-d", "--daemon", default=False, is_flag=True,
-                  help="Run in daemon mode.")
+    @click.option("-d", "--daemon", default=False, is_flag=True, help="Run in daemon mode.")
     def run(daemon):
+        """
+        Run the service.
+        """
         if daemon:
-            pid_file = './statsE16.pid'
+            pid_file = './revE16.pid'
             if os.path.isfile(pid_file):
                 pid = int(open(pid_file).read())
                 os.remove(pid_file)
@@ -71,12 +72,12 @@ if __name__ == '__main__':
                 except:
                     pass
             try:
-                p = subprocess.Popen(['python3', 'statsE16-server.py'])
+                p = subprocess.Popen(['python3', 'revE16-server.py'])
                 open(pid_file, 'w').write(str(p.pid))
             except subprocess.CalledProcessError:
-                raise ValueError("error starting statsE16-server.py daemon")
+                raise ValueError("error starting revE16-server.py daemon")
         else:
             print("Server running...")
-            app.run(host='0.0.0.0', port=7016)
+            app.run(host='0.0.0.0', port=7017)
 
     run()
---
+++ revE16.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class RevE16:
+    """
+    Revenue gathering logic.
+    """
+
+    def __init__(self):
+        """
+        Constructor for the Rev class.
+        """
+        return
